038d57a055bc62e9b7634114e5a443553dd84458
Matt Roberts <matt.roberts2@digital.hmrc.gov.uk>
2019-01-04 16:58:24 +0000

    WIP

--- app/uk/gov/hmrc/vatregisteredcompaniesstub/models/Payload.scala
+++ app/uk/gov/hmrc/vatregisteredcompaniesstub/models/Payload.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 HM Revenue & Customs
+ * Copyright 2019 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,8 +19,8 @@ package uk.gov.hmrc.vatregisteredcompaniesstub.models
 import play.api.libs.json.{Json, OFormat}
 
 case class Payload(
-  createsAndUpdates: List[VatRegisteredCompany],
-  deletes: List[VatNumber]
+  createsAndUpdates: List[VatRegisteredCompany] = List.empty[VatRegisteredCompany],
+  deletes: List[VatNumber] = List.empty[VatNumber]
 )
 
 object Payload {
--- app/uk/gov/hmrc/vatregisteredcompaniesstub/services/DataGenerator.scala
+++ app/uk/gov/hmrc/vatregisteredcompaniesstub/services/DataGenerator.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 HM Revenue & Customs
+ * Copyright 2019 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ package uk.gov.hmrc.vatregisteredcompaniesstub.services
 import cats.implicits._
 import org.scalacheck.Gen
 import uk.gov.hmrc.smartstub._
-import uk.gov.hmrc.vatregisteredcompaniesstub.models.{Address, Payload, VatRegisteredCompany}
+import uk.gov.hmrc.vatregisteredcompaniesstub.models.{Address, Payload, VatNumber, VatRegisteredCompany}
 
 
 object DataGenerator {
