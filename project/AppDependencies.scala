e074879f8997c5a2866750ddc51bcb9673c16da2
jawadsaleem5747 <175114233+jawadsaleem5747@users.noreply.github.com>
2025-01-14 16:35:33 +0000

    DLS-11268: revert as before

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,13 +5,12 @@ object AppDependencies {
 
   private val playVersion = "play-30"
   private val bootstrapVersion = "9.6.0"
-  private val stubDataGenerator = "1.1.0"
 
   val compile = Seq(
     ws,
     "uk.gov.hmrc"    %% s"domain-$playVersion"            % "10.0.0",
     "uk.gov.hmrc"    %% s"bootstrap-backend-$playVersion" % bootstrapVersion,
-    "uk.gov.hmrc"    %% "stub-data-generator"             % stubDataGenerator,
+    "uk.gov.hmrc"    %% "stub-data-generator"             % "1.1.0",
     "com.github.fge" %  "json-schema-validator"           % "2.2.14",
     compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.14" cross CrossVersion.full),
     "com.github.ghik" % "silencer-lib"                    % "1.7.14" % Provided cross CrossVersion.full
@@ -19,7 +18,6 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc"            %% s"bootstrap-test-$playVersion" % bootstrapVersion,
-    "uk.gov.hmrc"    %% "stub-data-generator"             % stubDataGenerator,
     "org.scalatestplus"      %% "scalacheck-1-17"              % "3.2.18.0"
   ).map(_ % "test")
 }
