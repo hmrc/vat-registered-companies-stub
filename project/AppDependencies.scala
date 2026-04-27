7b70bb3eb3bb41146afd4f7de345b6588b7bc7c8
jawadsaleem5747 <175114233+jawadsaleem5747@users.noreply.github.com>
2026-04-27 15:32:43 +0100

    DLS-12991: [VAT REG Stub] Replace source of json-schema-validator dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
 
   private val playVersion = "play-30"
-  private val bootstrapVersion = "10.6.0"
+  private val bootstrapVersion = "10.7.0"
 
   val compile = Seq(
     ws,
